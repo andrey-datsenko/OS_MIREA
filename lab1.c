3d0a62a2256d89ed43ae4f20a5f96b803d04ef07
andrey-datsenko <html.delo@yandex.ru>
2015-11-01 21:33:09 +0400

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
@@ -11,22 +12,28 @@ int main()
 	{
 		case -1:exit(1);
 		case 0: {
+			printf("CHILD:   Я дочерний процесс. PID =%d\n", getpid());
 			char name[20]; 
 			FILE *CHILD; 
-			sprintf(name, "file_%d.txt", getppid());
+			sprintf(name, "file_%d.txt", getpid());
 			CHILD=fopen(name, "w"); 
 			fprintf(CHILD, "%d %d", getppid(), getpid());
 			fclose(CHILD);
+			printf("CHILD:   Я создал файл 'file_%d.txt' и записал в него PID и PPID\n\n", getpid());
 			exit(2);}
 		default: {
+			 printf("PARENTS: Я родительский процесс. PID =%d, CHILD PID=%d\n", getpid(), pid);
 			 char name2[20];
 			 FILE *PARENTS;
-			 sprintf(name2, "file_%d.txt", pid);
+			 sprintf(name2, "file_%d.txt", getpid());
 			 PARENTS=fopen(name2, "w"); 
 			 fprintf(PARENTS, "%d" , getpid());
 			 fclose(PARENTS);
+			
+			 printf("PARENTS: Я создал файл 'file_%d.txt' и записал в него PID и PID моего потомка\n", getpid());
+			 printf("PARENTS: Жду завершения потомка\n");
 			 wait(NULL);
-			 printf("\nGood\n\n");}
+			 printf("\nВсе good\n");}
 	}
 	return 0;
 }
